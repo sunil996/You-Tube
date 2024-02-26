ac3bf765a56677fb81c1372c9ce234ffbbf98660
sunil996 <sunilshekhawat772@gmail.com>
2024-02-27 01:23:24 +0530

    Remove unnecessary console.log statement from util file

--- src/utils/utilFunctions.js
+++ src/utils/utilFunctions.js
@@ -3,8 +3,8 @@ const getPublicIdFromUrl = (url) => {
     
     const urlParts = url.split('/');
     const filenameWithExtension = urlParts[urlParts.length - 2] + "/" + urlParts[urlParts.length - 1];
-    console.log(filenameWithExtension)
     const [publicId, _] = filenameWithExtension.split(".");
+    console.log(publicId)
     return publicId;
 }
 
