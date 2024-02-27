37b100c5d0b139fa216315e207e7a5b0d10a285b
sunil996 <sunilshekhawat772@gmail.com>
2024-02-27 22:27:26 +0530

    remove : remove unncecessary console statement

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -12,8 +12,6 @@ const errorHandler = (err, req, res, next) => {
   }
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  
-  //console.log(err);
 
   res.status(statusCode).json({
   statusCode,
