6f4674f2605a63a85882b26059e7b01a98ef098d
sunil shekhawat <sunilshekhawat772@gmail.com>
2024-03-19 11:32:16 +0530

    add : maliler service added

---
+++ src/utils/mailer.js
@@ -0,0 +1,12 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+
+    service: "gmail", 
+    auth: {
+      user:process.env.EMAIL_USER, 
+      pass:process.env.EMAIL_PASSWORD,
+    },
+  });
+  
+  module.exports = transporter;
